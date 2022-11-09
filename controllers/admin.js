813d45d77689106a5c4b2d58f54cd286eebf28a6
usipcomp <usipcompcentre2022august@gmail.com>
2022-11-09 11:13:16 +0530

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,3 +11,31 @@ module.exports.renderLoginForm = async (req,res) =>{
   res.render('admin/login');
    
 }
+
+
+
+module.exports.submitLoginForm = async (req,res) =>{
+  const {roll_no,password}= req.body;
+  //console.log(req.body);
+
+ const student = await Student.findOne({roll_no:roll_no.toLowerCase()});
+ //console.log(student);
+ if(!student){
+  req.flash('success','studentNOtFound');
+  return res.redirect('/student/login');
+ }
+ 
+ if(password==student.password){
+  //console.log("password matched");
+  const token = jwt.sign(
+    { _id: student._id },
+    "thisisasecretkeyhelloonetwothreefour"
+  );
+  res.cookie("token", token);
+
+  return res.redirect(`/student/home/${student._id}`);
+ }
+  else{
+  return res.redirect(`/student/login`);
+  }
+}
